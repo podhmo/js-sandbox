927f7481f4ee90d62bf074ec163046fb1451034d
podhmo <podhmo+altair@beproud.jp>
2015-12-29 00:11:45 +0900

    using node modules with typescript

--- typescript/multifiles/README.md
+++ typescript/multifiles/README.md
@@ -1,6 +1,36 @@
 - makeへの依存無くしたい
 
 ```
+npm install -g dtsm
+dtsm install
 make
 node ./lib/zip
+# or
+# FILE=src/zip make
 ```
+
+## hmm
+
+- requireを使ってnode_modulesを使う？
+- 分けたファイルをimportを使ってimport?
+- node.jsの機能を利用するコードを書いてみる
+
+## node.jsの機能を使う方法
+
+- Definetly Typedから `node.d.ts` を取得する
+
+## Definetly Typedを使う方法
+
+dtsmを使う？
+
+```
+npm install -g dtsm
+dtsm search node
+dtsm init
+dtsm install --save node
+```
+
+## Project Template 的なものは何を使うと良いんだろう？
+
+- [vvakame/typescript-project-sample](https://github.com/vvakame/typescript-project-sample)
+- .etc
--- typescript/multifiles/src/fizzbuzz/impl.ts
+++ typescript/multifiles/src/fizzbuzz/impl.ts
@@ -1,15 +1,15 @@
-export function fizzbuzz(from:number, to:number, cb: (string) => void){
-  for(let i=from; i<to; i++){
-    let arr = [];
-    if(i % 3 == 0){
-      arr.push("fizz");
+export function fizzbuzz(from:number, to:number, cb: (x:string) => void){
+    for(let i=from; i<to; i++){
+        let arr : string[] = [];
+        if(i % 3 == 0){
+            arr.push("fizz");
+        }
+        if(i % 5 == 0){
+            arr.push("buzz");
+        }
+        if(arr.length == 0){
+            arr.push(String(i));
+        }
+        cb(arr.join(""));
     }
-    if(i % 5 == 0){
-      arr.push("buzz");
-    }
-    if(arr.length == 0){
-      arr.push(String(i));
-    }
-      cb(arr.join(""));
-  }
 }
