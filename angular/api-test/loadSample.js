dd8238ede556e74e437b14fcb7b4d37d6e2b27f4
podhmo <podhmo+altair@beproud.jp>
2016-01-06 02:46:26 +0900

    angular using

--- angular/api-test/loadSample.js
+++ angular/api-test/loadSample.js
@@ -16,4 +16,13 @@ setup(function(){
   console.log('document is', typeof document);
   console.log('angular is', typeof angular);
   console.log('window.angular === angular', window.angular === angular);
+
+  angular.module('myApp', [])
+    .controller('helloController', ['$scope', function ($scope) {
+      $scope.title = 'Node!';
+    }]);
+
+  document.body.innerHTML = '<div ng-controller="helloController">{{ title }}</div>';
+  angular.bootstrap(document, ['myApp']);
+  console.log(angular.element(document.body).html());
 });
