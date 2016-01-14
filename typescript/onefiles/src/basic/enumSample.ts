4fa6bd50820b69968cb1aa8e51cbb958d3a0a798
podhmo <podhmo@be.jp>
2016-01-14 13:34:53 +0900

    enum sample

---
+++ typescript/onefiles/src/basic/enumSample.ts
@@ -0,0 +1,22 @@
+enum Color {
+  red, green, blue
+}
+console.log(JSON.stringify(Color, null, 2));
+
+/*
+{
+  "0": "red",
+  "1": "green",
+  "2": "blue",
+  "red": 0,
+  "green": 1,
+  "blue": 2
+}
+
+var Color;
+(function (Color) {
+    Color[Color["red"] = 0] = "red";
+    Color[Color["green"] = 1] = "green";
+    Color[Color["blue"] = 2] = "blue";
+})(Color || (Color = {}));
+*/
