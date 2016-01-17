c248fc27d4aaeecb931b76b4b41d3749957216e8
podhmo <podhmo+altair@beproud.jp>
2016-01-17 18:38:33 +0900

    gulp: browserify standalone + external

--- angular/gulp/gulpfile.js
+++ angular/gulp/gulpfile.js
@@ -7,12 +7,19 @@ var buffer = require('vinyl-buffer');
 var gutil = require('gulp-util');
 var sourcemaps = require('gulp-sourcemaps');
 
+gulp.task('vendor', function(){
+});
+
 gulp.task('browserify', function(){
   var b = browserify({
     entries: './src/js/app/index.js',
     debug: true,
+    standalone: "App",
     transform: [] // TODO: "browserify-shim"
   });
+
+  // todo: does not touch. gulp file.(using browserify-shim?)
+  b.external(require.resolve("angular"));
 
   return b.bundle()
     .pipe(source('app.js'))
