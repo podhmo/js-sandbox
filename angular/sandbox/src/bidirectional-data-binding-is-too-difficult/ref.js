04def7b4a19ad2272b8d1e029ad6d1d928d6b1d2
podhmo <podhmo+altair@beproud.jp>
2016-04-12 22:54:19 +0900

    simplify

--- angular/sandbox/src/bidirectional-data-binding-is-too-difficult/ref.js
+++ angular/sandbox/src/bidirectional-data-binding-is-too-difficult/ref.js
@@ -3,19 +3,6 @@ require("console-angular").setup((angular, document) => {
   class ChildController {
   }
 
-  class Child {
-    static define() {
-      return {
-        controller: ChildController,
-        controllerAs: "vm",
-        bindings: {
-          ob: "&"
-        },
-        template: `<pre>{{ ::vm.ob()|json }}</pre>`
-      };
-    }
-  }
-
   class ParentController {
     constructor($timeout) {
       console.log(`*****initialize*****`);
@@ -26,27 +13,29 @@ require("console-angular").setup((angular, document) => {
     }
   }
 
-  class Parent {
-    static define() {
-      return {
-        controller: ParentController,
-        controllerAs: "vm",
-        bindings: {
-        },
-        template: `<child ob="::vm.ob"></child>`
-      };
-    }
-  }
-
   angular.module("app", ["console"])
-    .component("child", Child.define())
-    .component("parent", Parent.define())
+    .component("child",   {
+      controller: ChildController,
+      controllerAs: "vm",
+      bindings: {
+        ob: "&"
+      },
+      template: `<pre>{{ ::vm.ob()|json }}</pre>`
+    })
+    .component("parent", {
+      controller: ParentController,
+      controllerAs: "vm",
+      bindings: {
+      },
+      template: `<child ob="::vm.ob"></child>`
+    })
   ;
 
   document.body.innerHTML = `<parent></parent>`;
   const inj = angular.bootstrap(document, ["ng", "app"]);
   const $timeout = inj.get("$timeout");
   inj.get("$rootScope").$apply();
+
   console.log(angular.element(document.body).html().toString());
   $timeout(() => {
     console.log(`*****timeout*****`);
