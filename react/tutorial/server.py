726dfb292fca9cfa09e41715ca7c4db81f726f0d
podhmo <podhmo+altair@beproud.jp>
2016-04-16 20:03:37 +0900

    tutorial 8

--- react/tutorial/server.py
+++ react/tutorial/server.py
@@ -1,16 +1,15 @@
 import json
+import cgi
+import random
 from wsgiref.simple_server import make_server
 
+
 def app(environ, start_response):
-    status = '200 OK'
     path = environ["PATH_INFO"]
     if path.startswith("/api"):
         return on_api(path, environ, start_response)
     else:
         return on_html(path, environ, start_response)
-    headers = [('Content-type', 'application/JSON; charset=utf-8')]
-    start_response(status, headers)
-    return [json.dumps(data)]
 
 
 def on_html(path, environ, start_response):
@@ -21,14 +20,24 @@ def on_html(path, environ, start_response):
         return [rf.read()]
 
 
+data = [
+    {"author": "random", "text": "{}".format(random.random())},
+    {"author": "Pete Hunt", "text": "This is one comment"},
+    {"author": "Jordan Walke", "text": "This is *another* comment"}
+]
+
+
 def on_api(path, environ, start_response):
     status = '200 OK'
     headers = [('Content-type', 'application/JSON; charset=utf-8')]
     start_response(status, headers)
-    data = [
-        {"author": "Pete Hunt", "text": "This is one comment"},
-        {"author": "Jordan Walke", "text": "This is *another* comment"}
-    ]
+
+    if environ["REQUEST_METHOD"] == "POST":
+        wsgi_input = environ["wsgi.input"]
+        form = cgi.FieldStorage(fp=wsgi_input, environ=environ, keep_blank_values=True)
+        data.append({k: form[k].value for k in form if k in ("author", "text")})
+    else:
+        data[0]["text"] = "{}".format(random.random())
     return [json.dumps(data)]
 
 
