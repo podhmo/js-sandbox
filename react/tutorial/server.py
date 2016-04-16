93b062d935813954235b9382d666398cfbbdcf50
podhmo <podhmo+altair@beproud.jp>
2016-04-17 02:02:50 +0900

    python3.x

--- react/tutorial/server.py
+++ react/tutorial/server.py
@@ -1,8 +1,29 @@
 import os.path
 import json
-import cgi
 import random
 from wsgiref.simple_server import make_server
+
+
+import sys
+PY3 = sys.version_info[0] == 3
+if PY3:
+    text_type = str
+    binary_type = bytes
+else:
+    text_type = unicode
+    binary_type = str
+
+
+def bytes_(s, encoding='utf-8', errors='strict'):
+    if isinstance(s, text_type):
+        return s.encode(encoding, errors)
+    return s
+
+
+def text_(s, encoding='utf-8', errors='strict'):
+    if isinstance(s, binary_type):
+        return s.decode(encoding, errors)
+    return s
 
 
 def app(environ, start_response):
@@ -21,12 +42,12 @@ def on_html(path, environ, start_response):
         with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), path.lstrip("/"))) as rf:
             status = '200 OK'
             start_response(status, headers)
-            return [rf.read()]
+            return [bytes_(rf.read())]
     except Exception as e:
         print(e)
         status = '404 Not Found'
         start_response(status, headers)
-        return [bytes(path)]
+        return [bytes_(path)]
 
 
 data = [
@@ -44,10 +65,10 @@ def on_api(path, environ, start_response):
     if environ["REQUEST_METHOD"] == "POST":
         wsgi_input = environ["wsgi.input"]
         content_length = int(environ["CONTENT_LENGTH"])
-        data.append(json.loads(wsgi_input.read(content_length)))
+        data.append(json.loads(text_(wsgi_input.read(content_length))))
     else:
         data[0]["text"] = "{}".format(random.random())
-    return [json.dumps(data)]
+    return [bytes_(json.dumps(data))]
 
 
 httpd = make_server('', 8000, app)
