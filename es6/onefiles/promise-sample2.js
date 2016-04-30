89dc90e74d5f7d16840e73e643ea47ad4bc43668
podhmo <podhmo+altair@beproud.jp>
2016-04-30 21:07:29 +0900

    hmm

--- es6/onefiles/promise-sample2.js
+++ es6/onefiles/promise-sample2.js
@@ -11,7 +11,7 @@ function toAsync(fn, d) {
     return new Promise((resolve) => {
       setTimeout(() => {
         return resolve(x);
-      });
+      }, d);
     }).then(fn);
   };
 }
@@ -26,7 +26,7 @@ function toFragile(fn, fragileP){
 
 function toRandomFragile(fn, n) {
   return toFragile(fn, () => {
-    return Math.random() > n;
+    return Math.random() <= n;
   });
 }
 
@@ -42,6 +42,21 @@ function withTick(fn, tickFn) {
     return fn(tickFn(x));
   };
 }
+
+function withRetry(gen, branch) {
+  return (value) => {
+    function loop(p, i, errors){
+      return p.catch((err) => {
+        errors.push(err);
+        let q = branch(() => { return gen(value); }, i, err);
+        return q !== void 0 ? loop(q, i + 1,  errors) : Promise.reject({value: value, errors: errors});
+      });
+    }
+    const p = Promise.resolve(value).then(gen);
+    return loop(p, 1, []);
+  };
+}
+
 
 function displayPromise(p) {
   return p.then((v) => {
@@ -61,14 +76,20 @@ ng: ${JSON.stringify(err, null, 2)}
 
 const st = process.hrtime();
 const incAPI = toAsync(withTick(toRandomFragile(inc, 0.7), tick(st)), 100);
+const incAPIRetry5 = withRetry(incAPI, (doRetry, i, err) => {
+  console.log(`\tretry ${i}: ${JSON.stringify(err)}`);
+  if (i < 5) {
+    return doRetry();
+  }
+});
 
 
-const p = Promise
-  .resolve(1)
-  .then(incAPI)
-  .then(incAPI)
-  .then(incAPI)
-  .then(incAPI)
-;
-
-displayPromise(p);
+(() => {
+  const p = Promise.resolve(1)
+        .then(incAPIRetry5)
+        .then(incAPIRetry5)
+        .then(incAPIRetry5)
+        .then(incAPIRetry5)
+  ;
+  displayPromise(p);
+})();
